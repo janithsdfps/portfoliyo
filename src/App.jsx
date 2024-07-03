bf89f90f70aa3b3805763e877916596eb03dd31c
L.V.C.Janith sirilal <janithcsv.com>
2024-07-03 12:53:06 +0530

    floders done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import Home from './pages/Home'
-import Portfolio from './pages/Portfolio'
-import NotFound from './pages/NotFound'
+import { BrowserRouter } from "react-router-dom";
 
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
-function App() {
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "../src/components";
 
+const App = () => {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Portfolio" element={<Portfolio />} />
-          <Route path="*" element={<Navigate to="/not-found" replace />} />
-          <Route path="/not-found" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </>
-  )
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
